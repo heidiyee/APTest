549f6bb0b951cbea368f50681d3f77b5b93c286a
Temp User <tempuser@client.openvpn.net>
2016-05-16 12:18:18 -0700

    Initial Commit

---
+++ AP Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AP Test
+//
+//  Created by Temp User on 5/16/16.
+//  Copyright © 2016 Temp User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AP TestTests/AP_TestTests.m
@@ -0,0 +1,39 @@
+//
+//  AP_TestTests.m
+//  AP TestTests
+//
+//  Created by Temp User on 5/16/16.
+//  Copyright © 2016 Temp User. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AP_TestTests : XCTestCase
+
+@end
+
+@implementation AP_TestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
